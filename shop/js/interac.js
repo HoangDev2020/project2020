e0374147dd4660b3e6f8773ffe6d6174278e80b4
unknown <poho1234569@gmail.com>
2020-11-14 19:10:20 -0500

    updated

--- shop/js/interac.js
+++ shop/js/interac.js
@@ -22,52 +22,52 @@ fetch('https://5f9e30996ee5fa00168a4eb5.mockapi.io/api/new' )
 
 
 let value = localStorage.getItem('indexOfChoosenProduct');
-function add_remove() {
-    let cart = document.getElementById('cart');
-    let count;
-    if (localStorage.length != 0) {
-        count = localStorage.getItem('numberOfProduct');
-        cart.textContent = 'Giỏ hàng' + ' ' + '(' + count + ')';
-    } else {
-        localStorage.setItem('numberOfProduct', 0);
-    }
+// function add_remove() {
+//     let cart = document.getElementById('cart');
+//     let count;
+//     if (localStorage.length != 0) {
+//         count = localStorage.getItem('numberOfProduct');
+//         cart.textContent = 'Giỏ hàng' + ' ' + '(' + count + ')';
+//     } else {
+//         localStorage.setItem('numberOfProduct', 0);
+//     }
     
-    let btn_add = document.getElementsByClassName('btn_add')
-    for (let i = 0; i < btn_add.length; i++) {
-        btn_add[i].addEventListener('click', function() {
-            let value = localStorage.getItem('indexOfChoosenProduct')
-            btn_add[i].setAttribute('disabled', 'true');
-            if(value == ''){
-                value +=i;
-            } else {
-                value += `,${i}`;
-            }
-            localStorage.setItem('indexOfChoosenProduct', value);
+//     let btn_add = document.getElementsByClassName('btn_add')
+//     for (let i = 0; i < btn_add.length; i++) {
+//         btn_add[i].addEventListener('click', function() {
+//             let value = localStorage.getItem('indexOfChoosenProduct')
+//             btn_add[i].setAttribute('disabled', 'true');
+//             if(value == ''){
+//                 value +=i;
+//             } else {
+//                 value += `,${i}`;
+//             }
+//             localStorage.setItem('indexOfChoosenProduct', value);
             
             
-            count++;
-            localStorage.setItem('numberOfProduct', count);
-            cart.textContent = 'Giỏ hàng' + ' ' + '(' + localStorage.getItem('numberOfProduct') + ')';
-        });
-    };
+//             count++;
+//             localStorage.setItem('numberOfProduct', count);
+//             cart.textContent = 'Giỏ hàng' + ' ' + '(' + localStorage.getItem('numberOfProduct') + ')';
+//         });
+//     };
 
     
-    let btn_remove = document.getElementsByClassName('btn_remove');
-    for (let i = 0; i < btn_remove.length; i++) {
-        btn_remove[i].addEventListener('click', () => {
+//     let btn_remove = document.getElementsByClassName('btn_remove');
+//     for (let i = 0; i < btn_remove.length; i++) {
+//         btn_remove[i].addEventListener('click', () => {
             
             
-            if (count != 0) {
-                count--;
-            localStorage.setItem('numberOfProduct', count);
-            cart.textContent = 'Giỏ hàng' + ' ' + '(' + localStorage.getItem('numberOfProduct') + ')';
-            }else {
-                alert("Out of stocks")
-            };   
-        });
-    };
-};
-add_remove();
+//             if (count != 0) {
+//                 count--;
+//             localStorage.setItem('numberOfProduct', count);
+//             cart.textContent = 'Giỏ hàng' + ' ' + '(' + localStorage.getItem('numberOfProduct') + ')';
+//             }else {
+//                 alert("Out of stocks")
+//             };   
+//         });
+//     };
+// };
+// add_remove();
 
 let toCart = document.getElementById('cart');
 
@@ -79,3 +79,38 @@ fetch('https://5f9e30996ee5fa00168a4eb5.mockapi.io/api/new' )
     .then(response => response.json())
     .then(data => gerProduct(data));
 
+// function add_remove(){
+//     let cart = document.getElementById('cart');
+//     let count;
+//     if (localStorage.length != 0) {
+//         count = localStorage.getItem('numberOfProduct');
+//         cart.textContent = 'Giỏ hàng' + ' ' + '(' + count + ')';
+//     } else {
+//         localStorage.setItem('numberOfProduct', 0);
+//     };
+//     $('.btn_add').click(function(){ 
+//         console.log('cl');
+//         let value = localStorage.getItem('indexOfChoosenProduct');
+//             // btn_add[i].setAttribute('disabled', 'true');
+//             if(value == ''){
+//                 value +=i;
+//             } else {
+//                 value += `,${i}`;
+//             }
+//             localStorage.setItem('indexOfChoosenProduct', value);
+//             count++;
+//             localStorage.setItem('numberOfProduct', count);
+//             cart.textContent = 'Giỏ hàng' + ' ' + '(' + localStorage.getItem('numberOfProduct') + ')';
+//     });
+//     $('.btn_remove').click(function(){ 
+//         if (count != 0) {
+//             count--;
+//         localStorage.setItem('numberOfProduct', count);
+//         cart.textContent = 'Giỏ hàng' + ' ' + '(' + localStorage.getItem('numberOfProduct') + ')';
+//         }else {
+//             alert("Out of stocks")
+//         };    
+//     });
+// };
+
+// add_remove()
